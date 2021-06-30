7640835d0919e03aef64680ba4c5eb0b683991fa
Biswajit Mondal <biswa.mondal.it@gmail.com>
2021-07-01 04:20:49 +0530

    UI changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stock-market-ui'`, () => {
+  it(`should have as title 'Angular10SocialLogin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('stock-market-ui');
+    expect(app.title).toEqual('Angular10SocialLogin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('stock-market-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular10SocialLogin app is running!');
   });
 });
